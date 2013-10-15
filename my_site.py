221b3e991911c6b8378f6f92e1f9c08da2b47f6a
Me 217662 <217662@briangriffith.nfshost.com>
2013-10-15 18:00:33 +0000

    Commits

--- my_site.py
+++ my_site.py
@@ -3,23 +3,23 @@ import cgi, cgitb
 from Site import Site
 
 def main():
-  my_site = new Site()
+  my_site = Site()
   site_init(my_site)
 
   # retrieve info
-   #form = cgi.FieldStorage()
+  form = cgi.FieldStorage()
 
   # run site
-  #if "req" in form:
-  #  display(my_site.run(form["req"].value))
-  display(my_suite.run("home"))
+  if "req" in form:
+    display(my_site.run(form["req"].value))
+  # display(my_site.run("home"))
 
 def site_init(my_site):  
-  my_site.set_template("assets/template.html")
+  my_site.set_template()
   my_site.set_import(("assets/style.css")) 
   my_site.add_page("Home", "content/home.html")
   my_site.add_page("Resume", "content/resume.html")
-  my_site.add_page("Projects", "ReColor It", "contents/recolorit.html")
+  my_site.add_page("ReColor It", "contents/recolorit.html", category="Projects")
 
 def display(message):
   print "Content-Type: text/html\n\n"
